0ebb99956c3479b8849b21d964ce252f8fbac713
Pinterid <david.pinterics@gmail.com>
2020-05-07 10:26:36 +0200

    Update structure
    The structure was updated.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let root = "https://api.github.com"
+let anon = "https://api.anonfiles.com/upload"
+
 let proxy = "https://cors.snek.at/"
 let talks = []
 let source = {
@@ -16,7 +18,6 @@ async function getJSON (path) {
     if (!res.ok) {
       throw new Error(res.statusText)
     }
-
     return res.json();
   })
 }
@@ -58,22 +59,68 @@ async function addToTalks (obj) {
   obj.forEach(item => {
     let talk = {
       name: item.name,
-      url: item.html_url.replace("/blob/", "/raw/"),
+      display: "https://docs.google.com/viewer?embedded=true&url=" + item.html_url.replace("/blob/", "/raw/"),
+      download: item.html_url.replace("/blob/", "/raw/"),
+      url: item.html_url,
       path: item.path,
       repository: {
         name: item.repository.name,
         fullName: item.repository.full_name,
         url: item.repository.html_url,
         avatarUrl: item.repository.owner.avatar_url,
+        owner: item.repository.owner.login,
+        description: item.repository.description,
       },
     }
     talks.push(talk);
   });
 }
 
+async function getDisplayUrl (url) {
+  fetch(proxy + url, {
+    method: 'GET',
+  }).then(async response => {
+    let text = await response.text();
+    let soup = new JSSoup(text);
+    let tag = soup.findAll("a")[1];
+
+    return tag.attrs.href;
+  })
+}
+
+async function uploadFile (files) {
+  let data = new FormData();
+
+  data.append('file', files[0]);
+  
+  fetch(proxy + anon, {
+    method: 'POST',
+    body: data,
+  }).then(async response => {
+    let json = await response.json();
+    let file = json.data.file;
+    let display = await this.getDisplayUrl(file.url.short);
+    let talk = {
+      name: file.metadata.name,
+      display: display,
+      url: file.url.short,
+      path: "",
+      repository: {
+        name: "",
+        fullName: "",
+        url: "",
+        avatarUrl: "current user avatar",
+        owner: "current user name",
+      },
+    }
+    talks.push(talk);
+  })
+}
+
 pathProvider(source).then(() => {
   console.log(talks);
 });
+
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
